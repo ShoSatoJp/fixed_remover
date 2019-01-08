0568d7f1dddab19078a94cc7a615c1b4b7fcbe71
佐藤翔 <shosatojp2001@outlook.jp>
2019-01-08 15:08:16 +0900

    -

--- fixed_remover.user.js
+++ fixed_remover.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Fixed Remover
 // @namespace    https://github.com/ShoSatoJp
-// @version      0.9
+// @version      0.10
 // @description  remove fixed elements.
 // @author       ShoSato
 // @match        *://*/*
